fc9f3e5be3de01ab5d171c784e35205876890bf7
Jose Luis Rojas Gomez <amarillo17cyl@gmail.com>
2026-03-17 14:12:43 -0600

    It was added examples for Consumer Groups and Rebalancing Strategy.

--- kafka-core/src/test/java/com/acme/messaging/kafka/core/TestProducer.java
+++ kafka-core/src/test/java/com/acme/messaging/kafka/core/TestProducer.java
@@ -2,6 +2,7 @@ package com.acme.messaging.kafka.core;
 
 import com.acme.messaging.kafka.core.helpers.StringHelper;
 import com.acme.messaging.kafka.core.producers.ProducerString;
+import com.acme.messaging.kafka.core.producers.ProducerStringWithKeys;
 import com.acme.messaging.kafka.core.producers.callbacks.CommonCallback;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,7 @@ class TestProducer {
 
     public static final String BOOTSTRAP_SERVER_HOST = "localhost:9092";
     public static final String TOPIC_NAME = "second_topic";
+    public static final String THIRD_TOPIC_NAME = "third_topic";
 
     @Test
     void testProducerWithoutCallbackSuccess() {
@@ -24,6 +26,14 @@ class TestProducer {
     void testProducerWithCallbackSuccess() {
         ProducerString<String, String> producer =
                 new ProducerString<>(BOOTSTRAP_SERVER_HOST, TOPIC_NAME, new CommonCallback());
+
+        producer.send(StringHelper.buildUUID());
+    }
+
+    @Test
+    void testProducerWithKeysSuccess() {
+        ProducerStringWithKeys<String, String> producer =
+                new ProducerStringWithKeys<>(BOOTSTRAP_SERVER_HOST, THIRD_TOPIC_NAME);
 
         producer.send(StringHelper.buildUUID());
     }
